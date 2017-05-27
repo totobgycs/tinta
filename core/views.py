4b4959832e3b6678a03f6a5985d96f58cdecbc3c
Ubuntu <ubuntu@ip-172-31-37-25.eu-west-1.compute.internal>
2017-05-27 13:38:52 +0000

    tickers (yahoo finance)

--- core/views.py
+++ core/views.py
@@ -5,12 +5,31 @@ from django.template import loader
 
 from .models import Operation
 
+from yahoo_finance import Share
+
 def index(request):
     template = loader.get_template('core/dashboard.html')
     operations = [
         {'name': op.name, 'details': 'blah'} for op in Operation.objects.all()
     ]
-    context = {'operations': operations}
+    symbols = [
+      'GOOG',
+      'AAPL',
+      'FB',
+      'GE',
+      '^ESX',
+      '^AEX',
+      '^GSPC',
+      'ISP.MI',
+      'ASML',
+      'RDSB.L',
+      'GBPEUR=X',
+      'EURUSD=X',
+    ]
+    shares = [Share(sb) for sb in symbols]
+    prices = [{'symbol': s.symbol, 'current_value': s.get_price()} for s in shares]
+
+    context = {'operations': operations, 'shares': prices}
     return HttpResponse(template.render(context, request))
 
 def operation(request, operation_id):
