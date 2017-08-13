a48d9bc0a6b9acf6d201b8ea089e1cd6012971ed
Ubuntu <ubuntu@ip-172-31-37-25.eu-west-1.compute.internal>
2017-08-13 20:48:37 +0000

    database url abstraction

--- tinta/settings.py
+++ tinta/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,6 +84,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Celery
