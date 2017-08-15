25a3663ae88c9e356d4e0081f141fea152db9a89
Ubuntu <ubuntu@ip-172-31-37-25.eu-west-1.compute.internal>
2017-08-15 19:47:57 +0000

    bugfix

--- tinta/settings.py
+++ tinta/settings.py
@@ -140,7 +140,4 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
